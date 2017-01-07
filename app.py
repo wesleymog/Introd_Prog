33b5e6a0d22ea0ab93206c2f84c5eedac9603df8
Wesley <wesley@localhost.localdomain>
2017-01-07 10:28:10 -0200

    Login sem auth e Cadastro com bug e em processo

--- app.py
+++ app.py
@@ -1,9 +1,53 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
+msg=''
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    erro = None
+    if request.method == 'POST':
+        if request.form['usuario'] != 'admin' or request.form['senha'] != 'admin':
+            erro = 'Sai daqui satanas.'
+        else:
+            return redirect(url_for('registro'))
+    return render_template('login.html', erro=erro)
+@app.route("/registro")
+def registro():
+    return render_template("registro.html")
+@app.route('/form',methods = ['POST', 'GET'])
+def form():
+   if request.method == 'POST':
+      try:
+         print 'Eis me aqui'
+         nome = request.form['nome']
+         dre = request.form['dre']
+         curso = request.form['curso']
+         telefone = request.form['telefone']
+         email=request.form['email']
+         endereco=request.form['endereco']
+         bairro=request.form['bairro']
+         cidade=request.form['cidade']
+         datagrad=request.form['datagrad']
+         localgrad=request.form['localgrad']
+         orientador=request.form['orientador']
+         coorientadores=request.form['coorientadores']
+         endereco=endereco+", "+bairro+", "+cidade
+         with sql.connect("Sistema.db") as con:
+            print 'Estou aqui'
+            cur = con.cursor()
+            cur.execute("INSERT INTO Aluno (DRE, DataGrad, LocalGrad, Orientador, Corientadores) VALUES (?,?,?,?,?)",(dre,datagrad,localgrad,Orientador,coorientadores) )
+            cur.execute("INSERT INTO InfoBasica (nome, endereco, tel, email) VALUES (?,?,?,?)",(nome,endereco,tel,email) )
+            print 'Estou aqui"
 
-@app.route("/")
-def hello_world():
-    return render_template("login.html")
+            con.commit()
+            print 'Estou aqui'
+            msg = "Record successfully added"
+      except:
+         print 'eis'
+         con.rollback()
+         msg = "error in insert operation"
 
+      finally:
+         return render_template("resultado.html",msg = msg)
+         con.close()
 app.run()
--- banco.py
+++ banco.py
@@ -9,7 +9,7 @@ print "Table created successfully"
 conn.execute('CREATE TABLE Professor (id INTEGER AUTO_INCREMENT,SIAPE INTEGER,Tipo VARCHAR, CodOrientados VARCHAR)')
 print "Table created successfully"
 
-conn.execute('CREATE TABLE Aluno (id INTEGER AUTO_INCREMENT, DRE INTEGER, Graduacao VARCHAR, Mestrado VARCHAR, Orientador VARCHAR,Corientadores VARCHAR, Ingresso VARCHAR,Qualificacao VARCHAR, CodDisc INTEGER, Bolsa VARCHAR, Ativo BOOLEAN, Paper VARCHAR)')
+conn.execute('CREATE TABLE Aluno (id INTEGER AUTO_INCREMENT, DRE INTEGER, DataGrad VARCHAR,LocalGrad VARCHAR, Mestrado VARCHAR, Orientador VARCHAR,Corientadores VARCHAR, Ingresso VARCHAR,Qualificacao VARCHAR, CodDisc INTEGER, Bolsa VARCHAR, Ativo BOOLEAN, Paper VARCHAR)')
 print "Table created successfully"
 
 conn.close()
